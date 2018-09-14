5fd4a6083d701fa0a346ed7019797d5ed1eb87e6
richtier <rikatee@gmail.com>
2018-09-14 20:04:20 +0100

    Add charting

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,55 @@
 import datetime
 
 from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf.urls import url
+from django.urls import reverse
 
-from core import models
+from core import charts, models
+
+
+class MilesPerGallonView(TemplateView):
+    template_name = 'core/miles-per-gallon.html'
+
+    def get_context_data(self, **kwargs):
+        chart = charts.MilesPerGallonChart(vehicle_id=self.kwargs['pk'])
+        return super(MilesPerGallonView, self).get_context_data(
+            chart=chart.render(),
+            opts=models.Vehicle._meta,
+            has_view_permission=True,
+            original='Miles per gallon',
+            **kwargs
+        )
 
 
 @admin.register(models.Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
     search_fields = ('name', 'registration',)
-    list_display = ('name', 'registration', 'year_manufactured',)
+    list_display = (
+        'name',
+        'registration',
+        'year_manufactured',
+        'miles_per_gallon_button'
+    )
+
+    def miles_per_gallon_button(self, obj):
+        return '<a href="{url}">View miles per gallon</a>'.format(
+            url=reverse("admin:miles-per-gallon", kwargs={'pk': obj.pk})
+        )
+
+    def get_urls(self):
+        return [
+            url(
+                r'^(?P<pk>[0-9]+)/miles-per-gallon/$',
+                self.admin_site.admin_view(
+                    MilesPerGallonView.as_view()
+                ),
+                name="miles-per-gallon"
+            ),
+        ] + super(VehicleAdmin, self).get_urls()
+
+    miles_per_gallon_button.short_description = ''
+    miles_per_gallon_button.allow_tags = True
 
 
 @admin.register(models.Fuel)
